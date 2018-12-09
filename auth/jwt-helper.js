4989778acdf0e7267b2faf3d56863029d5b27524
Tofael Ahmed <tofael.raju@gmail.com>
2018-12-09 22:35:36 +0600

    Fix expirein value

--- auth/jwt-helper.js
+++ auth/jwt-helper.js
@@ -2,6 +2,5 @@ const jwt = require("jsonwebtoken");
 const jwtSecret = process.env.JWT_SECRET || "jwtSecret";
 const expiresIn = process.env.TOKEN_EXPIRES_IN_SECONDS || 3600;
 module.exports.signJWT = payload => {
-  payload.expiresIn = expiresIn;
-  return jwt.sign(payload, jwtSecret);
+  return jwt.sign(payload, jwtSecret, { expiresIn });
 };
