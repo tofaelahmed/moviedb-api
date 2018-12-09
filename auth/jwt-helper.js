f694f91b8aade3fea4ccba17bb18b81fadbd0373
Tofael Ahmed <tofael.raju@gmail.com>
2018-12-09 22:59:58 +0600

    Fix expire time

--- auth/jwt-helper.js
+++ auth/jwt-helper.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const jwtSecret = process.env.JWT_SECRET || "jwtSecret";
-const expiresIn = process.env.TOKEN_EXPIRES_IN_SECONDS || 3600;
+const expiresIn = process.env.TOKEN_EXPIRES_IN || "10h";
 module.exports.signJWT = payload => {
   return jwt.sign(payload, jwtSecret, { expiresIn });
 };
